8e800f205ccf3c09dd8be8071129e6c752acdd91
Roger <roger.walt@gmail.com>
2013-06-12 13:11:54 +0200

    first plot

--- results/sortdata.py
+++ results/sortdata.py
@@ -7,4 +7,10 @@ data.data.sort(key=lambda tup: tup[0])
 
 # plot cluster size
 pltdata = [(data.data[x][0], data.data[x][1]['clusterSize']['mean'], data.data[x][1]['clusterSize']['stderr'][5]) for x in range(801)]
-plt.plot([x for (x,y,z) in pltdata], [y for (x,y,z) in pltdata])
+plt.errorbar([x for (x,y,z) in pltdata], [y for (x,y,z) in pltdata], [z for (x,y,z) in pltdata])
+
+# plot energy
+pltdata = [(data.data[x][0], data.data[x][1]['energy']['mean'], data.data[x][1]['energy']['stderr'][5]) for x in range(801)]
+
+# plot magnetization
+pltdata = [(data.data[x][0], data.data[x][1]['magnetization']['mean'], data.data[x][1]['magnetization']['stderr'][5]) for x in range(801)]
