c1c78b3aa480ca14edfc4d4888301f970678822b
Roger <roger.walt@gmail.com>
2013-06-14 17:46:41 +0200

    autocorrelation plots

--- results/measurements/plot.py
+++ results/measurements/plot.py
@@ -57,6 +57,37 @@ def plotClusterSize():
     pl.legend(loc='lower right')
     pl.show()
     
+def plotComparisonAutoCorrelation(measure):
+    pltdataSingle = [(data.data[x]['systemSize'], data.data[x]['temperature'], data.data[x]['results']['single'][measure]['autocorr'][4]) for x in range(len(data.data))]
+    pltdataSingle.sort(key=lambda x: (x[0], x[1]))
+
+    pltdataWolff = [(data.data[x]['systemSize'], data.data[x]['temperature'], data.data[x]['results']['wolff'][measure]['autocorr'][4]) for x in range(len(data.data))]
+    pltdataWolff.sort(key=lambda x: (x[0], x[1]))
+
+    # initialize figure
+    fig = pl.gcf()
+
+    # initialize subplout
+    ax1 = pl.subplot(211)
+
+    for systemSize in sorted(list(set([a for (a,x,y) in pltdataSingle]))):
+        pl.plot([x for (a,x,y) in pltdataSingle if a==systemSize],[y for (a,x,y) in pltdataSingle if a==systemSize], label=systemSize)
+
+    pl.subplot(212, sharex = ax1)
+    for systemSize in sorted(list(set([a for (a,x,y) in pltdataWolff]))):
+        pl.plot([x for (a,x,y) in pltdataWolff if a==systemSize],[y for (a,x,y) in pltdataWolff if a==systemSize], label=systemSize)
+
+    pl.xlabel(xlbl)
+    fig.suptitle('Autocorrelation: '+title, fontsize=14)
+    ax1.legend(bbox_to_anchor=(1.01, 0), loc='lower left', borderaxespad=0.)
+    pl.show()
+
+# plot autocorrelations
+title='Energy'
+plotComparisonAutoCorrelation('energy')
+
+title='Magnetization'
+plotComparisonAutoCorrelation('magnetization')
 
 # plot energy
 title='Energy'
