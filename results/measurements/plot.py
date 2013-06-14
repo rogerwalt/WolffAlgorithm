6b00d999fd301324531ef84d41bce23813b00a6b
Roger <roger.walt@gmail.com>
2013-06-14 18:09:31 +0200

    plot: relative cluster size

--- results/measurements/plot.py
+++ results/measurements/plot.py
@@ -56,7 +56,22 @@ def plotClusterSize():
     pl.xlabel(xlbl)
     pl.legend(loc='lower right')
     pl.show()
-    
+   
+
+def plotClusterSizeRelative():
+    pltdataSingle = [(data.data[x]['systemSize'], data.data[x]['temperature'], data.data[x]['results']['wolff']['clusterSize']['mean'], data.data[x]['results']['wolff']['clusterSize']['stderr'][5]) for x in range(len(data.data))]
+    pltdataSingle.sort(key=lambda x: (x[0], x[1]))
+
+    for systemSize in sorted(list(set([a for (a,x,y,z) in pltdataSingle]))):
+        pl.errorbar([x for (a,x,y,z) in pltdataSingle if a==systemSize],[y/(a**3) for (a,x,y,z) in pltdataSingle if a==systemSize],[z/(a**3) for (a,x,y,z) in pltdataSingle if a==systemSize], label=systemSize)
+
+    fig = pl.gcf()
+    fig.suptitle(title,fontsize=14)
+    pl.xlabel(xlbl)
+    pl.legend(loc='lower left')
+    pl.show()
+
+ 
 def plotComparisonAutoCorrelation(measure):
     pltdataSingle = [(data.data[x]['systemSize'], data.data[x]['temperature'], data.data[x]['results']['single'][measure]['autocorr'][4]) for x in range(len(data.data))]
     pltdataSingle.sort(key=lambda x: (x[0], x[1]))
@@ -81,6 +96,10 @@ def plotComparisonAutoCorrelation(measure):
     fig.suptitle('Autocorrelation: '+title, fontsize=14)
     ax1.legend(bbox_to_anchor=(1.01, 0), loc='lower left', borderaxespad=0.)
     pl.show()
+
+# plot cluster size
+title='Relative Cluster size'
+plotClusterSizeRelative()
 
 # plot autocorrelations
 title='Energy'
